451afe7603f52a20d64fff2cbe4c93784eb87c17
KoichiEndo323 <waddledee323@gmail.com>
2023-08-19 19:08:41 +0900

    "first commit"

---
+++ Practice_todo/Practice_todo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice_todo
+//
+//  Created by 遠藤幸一 on 2023/08/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
